35a328a10a26b98b5fc79422da57ae8cd317346a
Brian Ellis <brian@briansresume.com>
2021-06-09 12:25:46 -0400

    fixing version compliancy issue

--- config/auth.php
+++ config/auth.php
@@ -4,7 +4,7 @@ return [
   'sso' => [
     'driver'  => 'sso',
     'model'   => \App\User::class
-  ]
+  ],
   /*
   |--------------------------------------------------------------------------
   | Authentication Defaults
