a8a0b5824535d45e635c53405384fcef80401d35
Brian Ellis <brian@briansresume.com>
2021-06-22 11:53:27 -0400

    policy principal fix

--- app/User.php
+++ app/User.php
@@ -56,7 +56,7 @@ class User extends Authenticatable implements MustVerifyEmail
 	// THIS IS REFERENCING A JOIN TABLE
 	public function policies()
 	{
-		return $this->belongsToMany(BondPolicies::class,'bond_policy_principals','user_id','policy_id');
+		return $this->belongsToMany(BondPolicies::class,'bond_policy_principals');
 	}
 
 	public function usergroup()
