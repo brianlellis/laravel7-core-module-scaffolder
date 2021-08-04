86ebe8f1119a54e1d2dd92b0e3dd6e97ad66fc37
Brian Ellis <brian@briansresume.com>
2021-08-04 09:21:22 -0400

    fixed issue with policy attachment payment method

--- app/Rapyd/System/Blade/Admin/pdf/policy-invoice.blade.php
+++ app/Rapyd/System/Blade/Admin/pdf/policy-invoice.blade.php
@@ -27,8 +27,15 @@
     </strong></p>
 
     {{$obligee['name']}}<br>
-    {{$obligee['address_street']}}<br />
-    {{$obligee['address_city']}}, {{$obligee['address_state']}} {{$obligee['address_zip']}}
+    @if (isset($obligee['address_street']))
+      {{$obligee['address_street']}}<br />
+    @endif
+    @if (isset($obligee['address_city']) && isset($obligee['address_state']) && isset($obligee['address_zip']))
+      {{$obligee['address_city']}}, {{$obligee['address_state']}} {{$obligee['address_zip']}}
+    @elseif (isset($obligee['address_state']))
+      {{$obligee['address_state']}}
+    @endif
+    
   </div>
 
   {{-- PRINCIPAL INFO --}}
