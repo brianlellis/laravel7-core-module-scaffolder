59f4ec68a81270dc353036501c3dec7b2a36f3cf
Alexey Filatov <alexey@jetsurety.com>
2021-08-18 16:29:46 +0300

    Fix session error on fake system time

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,7 +26,17 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
       if (config('app.env') != 'production' && ($system_test_time = config('app.system_test_time'))) {
-        Carbon::setTestNow(Carbon::parse($system_test_time));
+        $real_time = \Carbon\Carbon::now();
+        $test_time = \Carbon\Carbon::parse($system_test_time);
+
+        Carbon::setTestNow($test_time);
+
+        // extend session lifetime so that the current session doesn't expire until the fake current time
+        $diff_min = $real_time->diffInMinutes($test_time);
+        if ($diff_min > 0) {
+            $org_lifetime = Config::get('session.lifetime');
+            Config::set('session.lifetime', $diff_min + $org_lifetime);
+        }
       }
     }
 }
